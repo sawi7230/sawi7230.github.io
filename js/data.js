e317a69071eec13de1df4939d373c718fe4f6049
sawi7230 <sara.winter@esolutions.de>
2019-02-10 13:36:20 +0100

    fix styling, refactor main app

--- js/data.js
+++ js/data.js
@@ -2,7 +2,6 @@ const ainSoph = {
 	name: "Ain Soph",
 	lng: 139.7679399,
 	lat: 35.6702824,
-	description: "totemo oishii tabemono (^_^)b",
 	category: "restaurant",
 	foursquareId: "513b0f65e4b0e5f9d8158656"
 };
@@ -11,7 +10,6 @@ const tsTanTan = {
 	name: "T's Tan Tan",
 	lng: 139.7669222,
 	lat: 35.6797559,
-	description: "suupa ramen",
 	category: "restaurant",
 	foursquareId: "4d9d2d0c79d1ba7ac0fd3388"
 };
@@ -20,7 +18,6 @@ const lifeSupermarket = {
 	name: "Life",
 	lng: 139.8134605,
 	lat: 35.7103099,
-	description: "okii suupaa",
 	category: "supermarket",
 	foursquareId: "56612c17498efeaff6428a31"
 };
@@ -29,7 +26,6 @@ const yoyogiKoen = {
 	name: "Yoyogi Koen",
 	lng: 139.7021845,
 	lat: 35.669695,
-	description: "kawaii koen",
 	category: "park",
 	foursquareId: "4b5a3a54f964a520cdb528e3"
 };
@@ -38,7 +34,6 @@ const shinjukuGyoenKoen = {
 	name: "Shinjuku Gyoen Koen",
 	lng: 139.7093474,
 	lat: 35.6833866,
-	description: "okii koen",
 	category: "park",
 	foursquareId: "4b5ff761f964a520f1d129e3"
 };
